83739c562908e146ae34ec9cb119fa9d9aeb4800
Ridek240 <ridek240zwami@gmail.com>
2025-06-16 19:04:24 +0200

    up

--- RunTime/CommandArtibute.cs
+++ RunTime/CommandArtibute.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using UnityEngine;
-using UnityEngine.InputSystem.Android;
 
 public class CommandAttribute : Attribute
 {
